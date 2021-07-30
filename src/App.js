d0259d99c77f91d85ea9128b3b6b12864670b8dd
Hen <bitonhen5@gmail.com>
2021-07-30 16:57:57 +0300

    changed app text again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hen was here for tests!
+          Liraz the baby!
         </p>
         <a
           className="App-link"
